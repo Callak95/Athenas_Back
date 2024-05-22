43909dda003c32bdcefdd8b480233c0e3e96c359
Lucas Valente <lucasvalente95@gmail.com>
2024-05-21 22:32:36 -0300

    fix: Ajustando estrutura básica

--- athenas_backend/settings.py
+++ athenas_backend/settings.py
@@ -80,13 +80,6 @@ WSGI_APPLICATION = 'athenas_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-DATABASES = {
-    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'athenas_db',
         'USER': 'postgres',
